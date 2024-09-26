b6035f0384745c5ab4c1296f6c38c592acc4ce48
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-26 00:00:05 +0000

    creates my root path

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from typing import List
+from models import User
+from uuid import uuid4
 
 app= FastAPI()
 
+
+# db: List[User] = [
+#     User(
+#         id=uuid4(),
+#          first_name="Nobel"
+#          last_name="Fiawornu"
+#          gender=Gender.male,
+         
+
+
+
+#     )
+
+# ]
 @app.get('/')
 async def root():
     return {"message": "Hello Nobel"}
