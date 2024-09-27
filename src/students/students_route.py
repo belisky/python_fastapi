5062e7f22136fec2422d7d49e764fe6e7fe53f1a
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-27 03:06:02 +0000

    adds a route to get a single student by email

--- src/students/students_route.py
+++ src/students/students_route.py
@@ -1,10 +1,14 @@
 from typing import Optional, List
 from fastapi import APIRouter
-from src.students import students_models
+from permission_request.src.students import students_model
 from src.students.students_schema import Student
 
 students_router = APIRouter(prefix="/students")
 
 @students_router.get("/")
-async def get_all_users(limit: Optional[int] = 10, offset: Optional[int] = 0):
-    return await students_models.get_all_students(limit, offset)
+async def get_all_students(limit: Optional[int] = 10, offset: Optional[int] = 0):
+    return await students_model.get_all_students(limit, offset)
+
+@students_router.get("/{email}")
+async def get_student_by_email(email: str):
+    return await students_model.get_student_by_email(email)
