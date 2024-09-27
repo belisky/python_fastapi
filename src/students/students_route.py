8b21e4808fbcf222514776444a160d2a4857f4dc
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-27 03:08:11 +0000

    adds a post method to insert student

--- src/students/students_route.py
+++ src/students/students_route.py
@@ -12,3 +12,7 @@ async def get_all_students(limit: Optional[int] = 10, offset: Optional[int] = 0)
 @students_router.get("/{email}")
 async def get_student_by_email(email: str):
     return await students_model.get_student_by_email(email)
+
+@students_router.post("/")
+async def insert_student(student: Student):
+    return await students_model.insert_student(student)
