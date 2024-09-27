28230576f1230491382ffa25e323a6a3c12ce07b
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-27 03:18:48 +0000

    adds both students and trainers rout to main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from src.commons.postgres import database
-
+from src.students.students_route import students_router
+from src.trainers.trainers_route import trainers_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +14,8 @@ async def lifespan(app: FastAPI):
 
  
 app = FastAPI(lifespan=lifespan)
+app.include_router(students_router)
+app.include_router(trainers_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0")
