636602ab30b5896a7409ef19587dec897f1a203f
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-10-01 21:17:00 +0000

    comments out pydantic models

--- src/trainers/trainers_schema.py
+++ src/trainers/trainers_schema.py
@@ -1,33 +1,35 @@
-from pydantic import BaseModel
-from typing import Optional
-from enum import Enum
+# from pydantic import BaseModel
+# from typing import Optional
+# from enum import Enum
 
 
-class Gender(str,Enum):
-    male="male"
-    female="female"
+# class Gender(str,Enum):
+#     male="male"
+#     female="female"
 
-class Role(str,Enum):
-    student="student"
-    trainer="trainer"
+# class Role(str,Enum):
+#     student="student"
+#     trainer="trainer"
 
-class Courses(str,Enum):
-    sa="solutions architect"
-    cp="cloud practitioner"
+# class Courses(str,Enum):
+#     sa="solutions architect"
+#     cp="cloud practitioner"
 
-class Certificates(str,Enum):
-    sa="solutions architect associate"
-    cp="cloud practitioner certified"
-    sap="solutions architect professional"
+# class Certificates(str,Enum):
+#     sa="solutions architect associate"
+#     cp="cloud practitioner certified"
+#     sap="solutions architect professional"
 
 
 class Trainer:
-    first_name=str
-    last_name=str
-    middle_name=Optional[str]
-    email=str
-    gender=str
-    cohort=Optional[str]
-    role=Role.trainer
-    course=Courses
-         
+    def __init__(self,first_name=None,last_name=None,middle_name=None,email=None,gender=None,id=None):
+        self.id=id
+        self.first_name=first_name
+        self.last_name=last_name
+        self.middle_name=middle_name
+        self.email=email
+        self.gender=gender
+        
+        
+    def __repr__(self):
+        return f'Trainer({self.id},\'{self.first_name}\',\'{self.last_name}\',\'{self.middle_name}\',\'{self.email}\',\'{self.gender}\')'
