74bb030409134831e47ee9e2346107d2a9434e1f
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-10-01 21:16:27 +0000

    renames trainers_route to trainers_router

--- src/trainers/trainers_model.py
+++ src/trainers/trainers_model.py
@@ -3,19 +3,21 @@ from src.commons.postgres import database
 from src.trainers.trainers_schema import Trainer
 
 async def get_trainer_by_email(email: str) -> Optional[Trainer]:
-    query = "SELECT name, email FROM trainers WHERE email = $1"
+    query = "SELECT first_name, email FROM trainers WHERE email = $1"
     async with database.pool.acquire() as connection:
         row = await connection.fetchrow(query, email)
         if row is not None:
-            trainer = trainer(name=row["first_name"]+row["last_name"], email=row["email"]) 
+            
+            trainer = trainer(first_name=row["first_name"], email=row["email"]) 
             return trainer
         return None
 
 async def get_all_trainers(limit: int, offset: int) -> List[Trainer]:
-    query = "SELECT name, email FROM trainers LIMIT $1 OFFSET $2"
+    query = "SELECT first_name, email FROM trainers LIMIT $1 OFFSET $2"
     async with database.pool.acquire() as connection:
         rows = await connection.fetch(query, limit, offset)
-        trainers = [Trainer(name=record["first_name"]+record["last_name"], email=record["email"]) for record in rows]
+        print(rows)
+        trainers = [Trainer(first_name=record["first_name"], email=record["email"]) for record in rows]
         return trainers
     
 async def insert_trainer(trainer: Trainer):
